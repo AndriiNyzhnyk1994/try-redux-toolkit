304297a4a2d0bfab674d717bbdef935592490cea
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-09-08 13:00:58 +0300

    added state, 2 functions and some markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+export type ToDoType = {
+  id: string
+  title: string
+  completed: boolean
+}
+
 function App() {
+
+  const [todos, setTodos] = useState<ToDoType[]>([])
+  const [text, setText] = useState('')
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      const todo: ToDoType = {
+        id: new Date().toISOString(),
+        title: text,
+        completed: false
+      }
+      setTodos([todo, ...todos])
+    }
+    setText('')
+  }
+
+  const removeTodo = (toDoId: string) => {
+    setTodos(todos.filter(t => t.id !== toDoId))
+  }
+
+  const changeTodoStatus = (toDoId: string, status: boolean) => {
+    const todo = todos.find(t => t.id === toDoId)
+    if (todo) {
+      todo.completed = status
+      setTodos([...todos])
+    }
+    
+  }
+
+
   return (
     <div className="App">
-      
+      <label htmlFor="">
+        <input value={text} onChange={(e) => { setText(e.currentTarget.value) }} />
+        <button onClick={addTodo}>Add todo</button>
+      </label>
+      <ul>
+        {
+          todos.map((t) => {
+            return (
+              <li key={t.id}>
+                <input
+                  type="checkbox"
+                  checked={t.completed}
+                  onChange={(e) => { changeTodoStatus(t.id, e.currentTarget.checked) }} />
+                <span>{t.title}</span>
+                <span onClick={() => removeTodo(t.id)} className='deleter'>&times;</span>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
