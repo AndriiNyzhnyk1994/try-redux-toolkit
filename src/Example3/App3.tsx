473005abecf88fb3cc579800b470292619c2905e
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-09-12 23:50:38 +0300

    created a little counter and RTK slice

--- src/Example3/App3.tsx
+++ src/Example3/App3.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App3() {
+
+    const [count, setCount] = useState<number>(0)
+
+    const increment = () => {
+        setCount(count => count + 1)
+    }
+
     return (
-        <div>App3</div>
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>INC</button>
+        </div>
     )
 }
---
+++ src/Example3/store3/store3.ts
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+export const countSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        count: 0
+    },
+    reducers: {
+        increment(state) {
+            state.count + 1
+        }
+    }
+})
